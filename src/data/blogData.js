a6ac9f751a9965e766debbf487cd61d8cf2e19d7
Abhishek Thakur <kumarcops@gmail.com>
2022-01-28 23:17:14 +0530

    data update

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -7,6 +7,14 @@ export const blogData = [
         image: 'https://miro.medium.com/max/1400/1*2COqks2irtRrAXbTj3fPXg.png',
         url: 'https://abhishekthakur0.medium.com/github-copilot-your-ai-pair-programmer-8532cb4b0556'
     },
+    {
+        id: 2,
+        title: 'Flutter — Auto Complete TextField',
+        description: 'With GitHub Copilot, get suggestions for whole lines or entire functions right inside your editor.',
+        date: 'Jan 27, 2022',
+        image: 'https://res.cloudinary.com/teepublic/image/private/s--gpc7TJTO--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1585726530/production/designs/8796655_0.jpg',
+        url: 'https://abhishekthakur0.medium.com/flutter-auto-complete-textfield-509b5fd7432e'
+    },
 
 ]
 
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Abhishek Thakur',
     title: "Software Designer & Developer",
     desciption: "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://scontent.fblr2-1.fna.fbcdn.net/v/t39.30808-6/271646347_5120467621296825_6793403640088406252_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=J0TAD1e9etoAX_CW-dl&_nc_ht=scontent.fblr2-1.fna&oh=00_AT_o8gFocm3Krs4H-p4_M3fduJtg2xGaJAwoJM5r6nkFQQ&oe=61F305DB',
+    image: 'https://firebasestorage.googleapis.com/v0/b/abhishek-thakur.appspot.com/o/Photos%2Fabhishek_thakur.png?alt=media&token=1f7f2f7b-c9c5-46c8-96a0-34e12a8c29be',
     resumePdf: resume
 }
